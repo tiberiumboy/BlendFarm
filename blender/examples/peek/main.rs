f4df619641ef83cc8cdbd05d91629f50a9e9396f
MegaMind <8661186+jordanbejar@users.noreply.github.com>
2024-12-01 08:37:15 -0700

    Code cleanup in peek example.

--- blender/examples/peek/main.rs
+++ blender/examples/peek/main.rs
@@ -1,38 +1,23 @@
-use std::{/*fs::OpenOptions, io::Write,*/ path::PathBuf};
-
-// use blend::Blend;
 use blender::manager::Manager as BlenderManager;
+use std::path::PathBuf;
 
 /// Peek into the blend file to see what's inside.
 fn main() {
-    /*
-    let blend = Blend::from_path("./examples/assets/test.blend").expect("Invalid blender path provided");
-    let mut file = OpenOptions::new().write(true).create(true).open("./log.txt").unwrap();
-
-    for obj in blend.instances_with_code(b"SC") {
-        // let loc = obj.get_f32_vec("loc");
-        // let name = obj.get("id").get_string("name");
-        // println!("\"{name}\" at {loc:?} | {obj:?}");
-        let data = format!("{obj:?}\n");
-        let _ = &file.write(data.as_bytes()).unwrap();
-        // might be interesting to investigate the OBCamera for properties?
-    }
-
-    */
     let args = std::env::args().collect::<Vec<String>>();
-    let _blend_path = match args.get(1) {
+    let blend_path = match args.get(1) {
         None => PathBuf::from("./examples/assets/test.blend"),
         Some(p) => PathBuf::from(p),
     };
-    // // we reference blender by executable path. Version will be detected upon running command process. (Self validation)
+
+    // we reference blender by executable path. Version will be detected upon running command process. (Self validation)
     let mut manager = BlenderManager::load();
-    let _blender = match manager.get_blenders().first() {
+    let blender = match manager.get_blenders().first() {
         Some(blender) => blender.to_owned(),
         None => manager.download_latest_version().unwrap(),
     };
 
-    // match blender.peek(blend_path) {
-    //     Ok(result) => println!("{:?}", &result),
-    //     Err(e) => println!("Error: {:?}", e),
-    // }
+    match blender.peek(blend_path) {
+        Ok(result) => println!("{:?}", &result),
+        Err(e) => println!("Error: {:?}", e),
+    }
 }
