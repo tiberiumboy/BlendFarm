888ac5428dd6f2ff298663cf0a006286c962906e
= <8661186+tiberiumboy@users.noreply.github.com>
2024-12-07 14:22:25 -0800

    Impl. node window updates

--- src/components/node_window.tsx
+++ src/components/node_window.tsx
@@ -1,16 +1,23 @@
 import { invoke } from "@tauri-apps/api/core";
-import { RenderNodeProps } from "./render_node";
+import { RenderNodeProps, ComputerSpec } from "./render_node";
 import { useState } from "react";
 import RenderNode from "./render_node";
 import { listen } from "@tauri-apps/api/event";
 
-export default function NodeWindow() {
-  const [nodes, setNodes] = useState<String[]>([]);
+export interface node {
 
-  //TODO: How can I unsubscribe this?
+}
+
+export default function NodeWindow() {
+  const [nodes, setNodes] = useState<RenderNodeProps[]>([]);
+
+  //TODO: Do I need this? I use this to make node reveal to the GUI to say Hey I'm connecting... Be patient.
   listen<string>('node_discover', (event: any) => {
     let tmp = [...nodes];
+    // once discover, parse the payload into the name field.
     let id = event.payload;
+    let prop: RenderNodeProps = { name: id };   
+    
     if (!tmp.includes(id)) {
       tmp.push(id);
     }
@@ -19,22 +26,37 @@ export default function NodeWindow() {
 
   listen('node_disconnect', (event: any) => {
     let tmp = [...nodes];
-    let result = tmp.filter((t) => t == event.payload);
-    setNodes(result);
+    let id = event.payload;
+    tmp.filter((t) => t.name == id);
+    setNodes(tmp);
   });
 
+  // This is later fetch after the node sends the host information about the specs.
   listen('node_identity', (event: any) => {
-    console.log(event);
+    // here we'll parse teh information into json format
+    // 0 is peer_id in base58, 1 is computer specs object
+    let id = event.payload[0];
+    // 1 is the computer spec payload
+    let spec: ComputerSpec = event.payload[1];
+
+    let node: RenderNodeProps = { name: id, spec };
+    
+    let tmp = [...nodes];
+    tmp.filter((t) => t.name === id);
+    tmp.push(node); 
+    console.log(tmp);
+    setNodes(tmp);
   })
 
+  // TODO: Find a way to make this node selectable, and refresh the screen to display node property and information (E.g. Blender preview window, Activity monitor, specs, files completed, etc.)
   function nodeWindow() {
     return (
       <div>
         {/* Show the activity of the computer progress */}
         <h2>Computer Nodes</h2>
         <div className="group" id="RenderNodes">
-          {nodes.map((node: String) =>
-            <div>{node}</div> // todo - find a way to simplify this message down to something simplier. 
+          {nodes.map((node) =>
+            <div>{RenderNode(node)}</div> 
           )}
         </div>
       </div>
--- src/components/render_node.tsx
+++ src/components/render_node.tsx
@@ -2,34 +2,31 @@ import { invoke } from "@tauri-apps/api/core";
 import { CiTrash, CiCircleMore } from "react-icons/ci";
 
 export interface RenderNodeProps {
-  name?: string;
-  host?: string;
-  onDataChanged?: () => void;
+  name: string;
+  spec?: ComputerSpec;
+}
+
+export type ComputerSpec = {
+  host: string;
+  cpu: string;
+  gpu?: string;
+  arch: string;
+  os: string;
+  cores: number;
+  memory: number;
 }
 
 export default function RenderNode(node: RenderNodeProps) {
-  const deleteNode = (e: any) =>
-    invoke("delete_node", { targetNode: node }).then(() =>
-      notifyDataChanged(e),
-    ); // then we should signal a refresh somehow?
-
-  const notifyDataChanged = (e: any) => {
-    if (node.onDataChanged != null) {
-      node.onDataChanged();
-    }
-  };
-
   return (
     <div key={"Node_" + node.name}>
       <table>
         <tbody>
           <tr>
-            <td style={{ width: "100%" }}>{node.name}</td>
-            <td>
-              <CiCircleMore />
-            </td>
-            <td>
-              <CiTrash onClick={deleteNode} />
+            <td style={{ width: "100%" }}>
+              {node.spec?.host ?? "Loading"}
+              <p>{node.spec?.core ?? ""}</p>
+              <p>{node.spec?.gpu ?? ""}</p>
+              <div>{node.spec?.os ?? ""}</div>
             </td>
           </tr>
         </tbody>
