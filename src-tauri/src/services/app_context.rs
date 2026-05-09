66f8d7a39151fb57cd9f3a2626dfc6eabd7a550b
8661186+tiberiumboy@users.noreply.github.com <8661186+tiberiumboy@users.noreply.github.com>
2026-05-09 12:09:28 -0700

    Server settings is automatically loaded when app context is initialized.

--- src-tauri/src/services/app_context.rs
+++ src-tauri/src/services/app_context.rs
@@ -1,18 +1,14 @@
-
-
 // Used to help organize dependency injections
 use crate::{BlenderManager, models::server_setting::ServerSetting};
 
 pub(crate) struct AppContext {
     pub manager: BlenderManager,
-    pub settings: ServerSetting,    // default ::load()
+    pub settings: ServerSetting, // default ::load()
 }
 
 impl AppContext {
-    pub fn new(manager: BlenderManager, settings: ServerSetting ) -> Self {
-        Self {
-            manager,
-            settings
-        }
+    pub fn new(manager: BlenderManager) -> Self {
+        let settings = ServerSetting::load();
+        Self { manager, settings }
     }
 }
